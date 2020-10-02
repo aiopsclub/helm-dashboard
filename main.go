60f381d93ce7e399bef5afda54836d3d26f2d68f
yxxhero <aiopsclub@163.com>
2020-10-02 13:51:42 +0800

    optimize code

--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func configMiddleware(r *gin.Engine) {
+	r.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
+
+	// custom log format
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.Format(time.RFC1123),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+
+	}))
+
+}
+
+func init() {
+	gin.DisableConsoleColor()
+
+}
 
 func main() {
 	r := gin.Default()
+	configMiddleware(r)
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
